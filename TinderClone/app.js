ffccd9b6581ad2e4b3611d4f2bf84dfe3da77106
yash_warhekar <yash2000warhekar@gmail.com>
2025-10-05 01:36:05 +0530

    init -database

--- TinderClone/app.js
+++ TinderClone/app.js
@@ -3,6 +3,27 @@ const express=require('express')
 const app=express()
 const {connectDB}=require('./src/config/database')
 
+const {User}=require('./src/models/user')
+
+app.post('/signup',async (req,res)=>{
+    const newuser=new User({
+        firstName:'bhavesh',
+        lastName:'dahikar',
+        emailId:'bhuvi@gmail.com',
+        password:'BD@123',
+        age:22,
+        gender:'Male'
+    })
+
+    try{
+        await newuser.save()
+        console.log('user saved')
+        res.send(`user saved successfully of ${newuser.firstName}`)
+    }catch(err){
+        res.status(400).send('err saving user to db'+err.message)
+    }
+    
+})
 
 
 
--- TinderClone/src/config/database.js
+++ TinderClone/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose =require('mongoose')
 
 //we are connecting cluster here
 const connectDB=async()=>{
-    await mongoose.connect("mongodb+srv://yash2000warhekar:Yash%402003@cluster0.fglfdqb.mongodb.net/")
+    await mongoose.connect("mongodb+srv://yash2000warhekar:Yash%402003@cluster0.fglfdqb.mongodb.net/TinderClone")
 }
 
 
