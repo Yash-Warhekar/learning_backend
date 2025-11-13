5f43da0c9a3f0f6990a3a03b455b1cce7d43e6ce
yash_warhekar <yash2000warhekar@gmail.com>
2025-11-13 10:36:26 +0530

    /user/feed

--- TinderClone/src/routes/user.js
+++ TinderClone/src/routes/user.js
@@ -2,6 +2,7 @@
 const express=require('express');
 const { userAuth } = require('../middlewares/auth');
 const ConnectionRequestModel = require('../models/connectionRequest');
+const { User } = require('../models/user');
 
 const userRouter=express.Router();
 
@@ -20,11 +21,17 @@ userRouter.get('/user/connections',userAuth,async(req,res)=>{
                 {fromUserId:loggedInUser._id,status:'accepted'}
             ]
         }).populate('fromUserId',userSavedData)
+        .populate('toUserId',userSavedData)
         
         if(conectionRequest.length<1){
             return res.status(404).json({message:'Currently No requests'})
         }
-        const data=conectionRequest.map((userdata)=>userdata.fromUserId)
+        const data=conectionRequest.map((userdata)=>{
+            if(userdata.fromUserId===loggedInUser._id){
+                return userdata.toUserId
+            }
+            return userdata.fromUserId
+        })
         res.json({data})
 
     }catch(err){
@@ -65,7 +72,36 @@ userRouter.get('/user/requests/received',userAuth,async(req,res)=>{
 
 
 // -get /user/feed - give you the profiles of other users on platform
+userRouter.get('/feed',userAuth,async(req,res)=>{
+    try{
+        const loggedInUser=req.user
 
+    //get all the connections of either sent/received by user 
+    const connectionRequest=await ConnectionRequestModel.fine({
+        $or:[{fromUserId:loggedInUser._id},{toUserId:loggedInUser._id}]
+    }).select('toUserId fromUserId');
+
+    //remove already connected users from feed
+    const hideUserFromFeed=new Set()
+
+    connectionRequest.forEach((request) => {
+        hideUserFromFeed.add(request.fromUserId.toString())
+        hideUserFromFeed.add(request.toUserId.toString())
+    });
+
+    //find user in db except already connected ones
+    const users=await User.find({
+        $and:[
+            {_id:{$nin:Array.from(hideUserFromFeed)}},
+            {_id:{$ne:loggedInUser._id}}
+        ]
+    }).select(userSavedData)
+
+    res.send(users)
+    }catch(err){
+        res.status(400).send('Error: '+ err.message)
+    }
+})
 
 
 
