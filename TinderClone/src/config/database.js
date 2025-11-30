35a16c789c8a0fed7bdf32a7172f32edd670d24f
yash_warhekar <yash2000warhekar@gmail.com>
2025-11-30 10:35:50 +0530

    moved tinder backend

--- TinderClone/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose =require('mongoose')
-
-//we are connecting cluster here
-const connectDB=async()=>{
-    await mongoose.connect("mongodb+srv://yash2000warhekar:Yash%402003@cluster0.fglfdqb.mongodb.net/TinderClone")
-}
-
-
-module.exports={connectDB}
